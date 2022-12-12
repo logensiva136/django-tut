e9703126a1bd958c5cbfe17900f5348e2ecd3499
Logentheran <logensiva136@gmail.com>
2022-12-12 12:37:59 +0800

    kakka

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("<int:month>", views.monthly_challenges),
+    path("<int:month>", views.monthly_challenges_by_number),
     path("<str:month>", views.monthly_challenges)
 ]
 
